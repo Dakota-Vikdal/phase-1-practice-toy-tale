5d583fdafc015befa7faf5dbe8ef3c0f30fdb68c
catherine <catmorillo@hotmail.com>
2023-02-07 11:04:19 -0500

    pushing

--- src/index.js
+++ src/index.js
@@ -16,19 +16,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const toyCollection = document.querySelector("#toy-collection")
+const h2 = document.createElement("h2");
+const img = document.createElement("img");
+const p = document.createElement("p");
+const button = document.createElement("button");
 
 
 function addNewToys() {
-  fetch('http://localhost:3000/toys')
+  fetch("http://localhost:3000/toys")
   .then(response => response.json())
   .then(data => renderToys(data))
 }
 addNewToys();
 
 function renderToys(toys) {
-  const divElement = document.createElement('div');
-  divElement.classList.add("card");
-  toyCollection.append(divElement);
-  
-  console.log(divElement);
+  toys.forEach(toy => { 
+    const divElement = document.createElement('div');
+    divElement.classList.add("card");
+    toyCollection.append(divElement);
+    divElement.append(h2);
+    console.log(h2);
+  })
 }
